fee90dbeceabaa7829f236cfe70c2fed4b5c446a
Jasen Lassig <35702819+JasenLassig@users.noreply.github.com>
2019-03-13 13:56:03 -0600

    fleshing out main

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class BlackJack extends Game {
 
-    public BlackJack() {
+    public BlackJack(int decks, int players) {
+        this.players = new ArrayList<Player>(players);
+        for (int i = 0; i < players; i++) {
+            this.players.add(new Player());
+        }
+        dealer = new Dealer();
+        deck = new GameDeck(decks);
+        discardPile = new Stack<Card>();
+    }
 
+    public static void main (String [] args) {
+        System.out.println("How many players?");
+        BlackJack game;
+        try {
+            int players = System.in.read();
+            System.out.println("How many decks?");
+            int decks = System.in.read();
+            game = new BlackJack(decks, players);
+        } catch (Exception e) {
+
+        }
+        while (true) {
+
+        }
     }
 }
